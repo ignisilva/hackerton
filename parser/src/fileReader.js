df433219e9ae260a5955d0cee3c6219a93d4e326
Songhareem <ignisilva92@gmail.com>
2022-02-23 07:20:21 +0900

    feat: localGrade / restaurant dummy data done

--- parser/src/fileReader.js
+++ parser/src/fileReader.js
@@ -1,9 +1,13 @@
+const selector = require("./selector");
+
 const date = new Date();
 
-const fileName = `hackerton_${date.getFullYear()}${
+const fileTitle = selector + "_";
+
+const fileName = `${fileTitle}${date.getFullYear()}${
   date.getMonth() < 10 ? "0" + (date.getMonth() + 1) : date.getMonth() + 1
 }${date.getDate()}`;
 
-const datas = require(`../jsonFiles/${fileName}`);
+const datas = require(`../jsonFiles/${fileName}.json`);
 
 module.exports = datas;
